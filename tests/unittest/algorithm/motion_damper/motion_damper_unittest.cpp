96cd628e89f2f57c02419c94813e6c5cc6e915d3
pllee4 <pinloon_0428@hotmail.com>
2021-06-24 22:32:31 +0800

    algorithm: add inverse acceleration test

--- tests/unittest/algorithm/motion_damper/motion_damper_unittest.cpp
+++ tests/unittest/algorithm/motion_damper/motion_damper_unittest.cpp
@@ -39,3 +39,18 @@ TEST(MotionDamper, AccelerateThenStop) {
         << motion_command_msg.linear.x;
   }
 }
+
+TEST(MotionDamper, AccelerateInverse) {
+  MotionCommandMsg motion_command_msg;
+  float input[] = {0.0, -1.0, -1.5, 0.0};
+  float output[] = {0, -MAX_LINEAR_ACCELERATE,
+                    -MAX_LINEAR_ACCELERATE * 2.0,
+                    -MAX_LINEAR_ACCELERATE * 2.0 + MAX_LINEAR_DECELERATE};
+  for (int i = 0; i < 4; ++i) {
+    motion_command_msg.linear.x = input[i];
+    MotionDamper(&motion_command_msg);
+    EXPECT_FLOAT_EQ(motion_command_msg.linear.x, output[i])
+        << "Test failed: expected " << output[i] << " but got "
+        << motion_command_msg.linear.x;
+  }
+}
