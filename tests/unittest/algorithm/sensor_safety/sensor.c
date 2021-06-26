e030a498df01184e1cfa6d778cf21e3b54eb1c93
pllee4 <pinloon_0428@hotmail.com>
2021-06-26 19:25:46 +0800

    algorithm: sensor_safety: add in CheckSensorAlive implementation

--- tests/unittest/algorithm/sensor_safety/sensor.c
+++ tests/unittest/algorithm/sensor_safety/sensor.c
@@ -11,14 +11,34 @@
 #include <string.h>
 
 bool InitSensor(Sensor *sensor) {
-  for (int i = 0; i < NUM_OF_SENSOR_DATA; i++)
+  for (int i = 0; i < NUM_OF_SENSOR_DATA; i++) {
     sensor->data[i] = 0.0;
+    sensor->time_stamp = 0;
+  }
 }
 
 void SetSensorData(Sensor *sensor, float sensor_data[]) {
   memcpy(sensor->data, sensor_data, (sizeof(float) * NUM_OF_SENSOR_DATA));
 }
 
-float *GetSensorData(Sensor *sensor) {
-  return sensor->data;
+void SetSensorTimeStamp(Sensor *sensor, uint32_t time_stamp) {
+  sensor->time_stamp = time_stamp;
+}
+
+float *GetSensorData(Sensor *sensor) { return sensor->data; }
+
+bool CheckSensorAlive(Sensor *sensor) {
+  static uint32_t prev_time_stamp;
+  static bool first_data = true;
+  if (!first_data) {
+    if (sensor->time_stamp - prev_time_stamp > SENSOR_STILL_ALIVE_TIME)
+      return false;
+  } else {
+    if (sensor->time_stamp != 0)  // once got data
+      first_data = false;
+    else  // sensor not yet alive
+      return false;
+  }
+  prev_time_stamp = sensor->time_stamp;
+  return true;
 }
--- tests/unittest/algorithm/sensor_safety/sensor.h
+++ tests/unittest/algorithm/sensor_safety/sensor.h
@@ -14,6 +14,7 @@ extern "C" {
 #endif
 
 #include <stdbool.h>
+#include <stdint.h>
 
 #define SENSOR_TASK_TIME 20          // ms
 #define SENSOR_STILL_ALIVE_TIME 100  // should be always updating within 100ms
@@ -21,11 +22,14 @@ extern "C" {
 
 typedef struct {
   float data[NUM_OF_SENSOR_DATA];
+  uint32_t time_stamp;
 } Sensor;
 
 bool InitSensor(Sensor *sensor);
 void SetSensorData(Sensor *sensor, float sensor_data[]);
+void SetSensorTimeStamp(Sensor *sensor, uint32_t time_stamp);
 float *GetSensorData(Sensor *sensor);
+bool CheckSensorAlive(Sensor *sensor);
 
 #ifdef __cplusplus
 }
