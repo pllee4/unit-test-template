1c02365270120164408957c03624b0d645ee3b9b
pllee4 <pinloon_0428@hotmail.com>
2021-06-26 19:42:36 +0800

    algorithm: sensor_safety: update unittest according to updated implementation for sensor
    Signed-off-by: Pin Loon Lee <pinloon_0428@hotmail.com>

--- tests/unittest/algorithm/sensor_safety/sensor_safety_unittest.cpp
+++ tests/unittest/algorithm/sensor_safety/sensor_safety_unittest.cpp
@@ -6,27 +6,42 @@
  * Copyright (c) 2021 Pin Loon Lee (pllee4)
  */
 
-#include "sensor.h"
-
 #include "gtest/gtest.h"
+
+#include "sensor.h"
 
 #define SENSOR_TEST_RUN_TIME 1000
 
-TEST(SensorSafety, GetData) {
-  float mock_data[] = {1.0, 1.0, 1.0, 1.0};
-  float *real_data;
-  real_data = getSensorTestData(mock_data);
-  for (int i = 0; i < NUM_OF_DATA; ++i) {
-    EXPECT_FLOAT_EQ(real_data[i], mock_data[i]);
+Sensor test_sensor;
+
+TEST(SensorSafety, InitSensor) {
+  InitSensor(&test_sensor);
+  float *sensor_data = GetSensorData(&test_sensor);
+  for (int i = 0; i < NUM_OF_SENSOR_DATA; ++i) {
+    EXPECT_FLOAT_EQ(sensor_data[i], 0);
   }
 }
 
-TEST(SensorSafety, SensorFault) {
-  uint16_t time_count = 0;
-  // check SENSOR_STILL_ALIVE_TIME + SENSOR_TASK_TIME must be <  SENSOR_TEST_RUN_TIME
+TEST(SensorSafety, SetSensorData) {
+  InitSensor(&test_sensor);
+  float sensor_dataset[] = {1.0, 1.0, 1.0, 1.0};
+  SetSensorData(&test_sensor, sensor_dataset);
+  float *sensor_data = GetSensorData(&test_sensor);
+  for (int i = 0; i < NUM_OF_SENSOR_DATA; ++i) {
+    EXPECT_FLOAT_EQ(sensor_data[i], 1.0);
+  }
+}
+
+TEST(SensorSafety, SensorAlive) {
+  uint32_t time_count = 0;
+  // ensure sensor alive time and update time is less than total run time
+  EXPECT_LE(SENSOR_STILL_ALIVE_TIME + SENSOR_TASK_TIME, SENSOR_TEST_RUN_TIME);
+  InitSensor(&test_sensor);
   while (time_count <= SENSOR_TEST_RUN_TIME) {
     time_count += SENSOR_TASK_TIME;
-    // sensor data fault
+    // set sensor timestamp should be called in placed where there is sensor
+    // data comes in in application code
+    SetSensorTimeStamp(&test_sensor, time_count);
+    EXPECT_TRUE(CheckSensorAlive(&test_sensor));
   }
-  time_count += SENSOR_TASK_TIME;
 }
