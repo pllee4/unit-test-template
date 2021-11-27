bb54ba54d1882b4a378c093f5ecbb694b02d9a68
Pin Loon Lee <pinloon_0428@hotmail.com>
2021-11-27 23:19:18 +0800

    template: grid: grid_unittest.cpp: add Exception test

--- template/grid/test/grid_unittest.cpp
+++ template/grid/test/grid_unittest.cpp
@@ -19,3 +19,17 @@ TEST(Grid, CopyConstructor) {
   int y{anotherIntGrid.at(0, 0).value_or(0)};
   EXPECT_EQ(y, 10);
 }
+
+TEST(Grid, Exception) {
+  Grid<int> myDoubleGrid{10, 10};
+  try {
+    myDoubleGrid.at(100, 0) = 10.0;
+  } catch (std::out_of_range const& err) {
+    EXPECT_EQ(err.what(), std::string("x exceeded width!"));
+  }
+  try {
+    myDoubleGrid.at(0, 100) = 10.0;
+  } catch (std::out_of_range const& err) {
+    EXPECT_EQ(err.what(), std::string("y exceeded height!"));
+  }
+}
